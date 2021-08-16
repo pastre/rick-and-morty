5c537303522208b3430c639391e6a9364d528c5e
Bruno Pastre <pastre68@gmail.com>
2021-08-16 09:18:24 -0300

    First commit

--- RickAndMorty/ViewController.swift
+++ RickAndMorty/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var charactersTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+final class XGoHorseCharacterCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+}
